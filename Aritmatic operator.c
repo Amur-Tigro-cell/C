85900cfafa79bb52b790ed5aef97550755fa71cc
Subro sutradhar <subroopu@gmail.com>
2025-03-11 23:13:03 +0600

    Create Aritmatic operator.c

---
+++ Aritmatic operator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    
+    int x=5;
+    int y=2;
+    
+    int z=x+y;
+    int z=x-y;
+    int z=x*y;
+    int z=x/y;
+    int z=x%y;
+    
+    printf("%d",z);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    
+    int x=5;
+    int y=2;
+    
+    x++;
+    y--;
+    printf("%d\n",x);
+    printf("%d\n",y);
+
+    return 0;
+}
