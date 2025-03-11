0c5d92900817bbfb5e4217d36ca4508389aef876
Subro sutradhar <subroopu@gmail.com>
2025-03-11 22:43:02 +0600

    Create Format specifiers .c

---
+++ Format specifiers .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    
+    float item1= 6.77;
+    float item2= 50.00;
+    float item3= 100.66;
+    
+    printf("Item 1: $%-8.2f\n",item1);
+    printf("Item 2: $%-7.2f\n",item2);
+    printf("Item 3: $%-9.2f\n",item3);
+    
+    
+
+
+
+    return 0;
+}
