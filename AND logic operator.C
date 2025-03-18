1bb824aa31db8f05f48af6837d8c588cc9b0acd2
Subro sutradhar <subroopu@gmail.com>
2025-03-19 02:26:00 +0600

    Create AND logic operator.C

---
+++ AND logic operator.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    float temp = 25;        // Typo fixed
+    bool sunny = true;
+
+    if(temp >= 0 && temp <= 30 && sunny){
+        printf("\nThe weather is good!\n");
+    }
+    else{
+        printf("\nThe weather is bad!\n");
+    }
+
+    return 0;
+}
