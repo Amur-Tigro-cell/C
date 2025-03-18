acdfbfa3f5d3bee854b1c3f9836edd863b637c70
Subro sutradhar <subroopu@gmail.com>
2025-03-19 02:41:09 +0600

    Create NOT logic function.C

---
+++ NOT logic function.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    bool sunny=true;        // Typo fixed
+
+    if(!sunny){
+        printf("\nThe weather is good!\n");
+    }
+    else{
+        printf("\nThe weather is bad!\n");
+    }
+
+    return 0;
+}
