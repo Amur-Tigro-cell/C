499641d6353a3e303e596841a7302e00e17993ca
Subro sutradhar <subroopu@gmail.com>
2025-03-22 23:56:52 +0600

    Create For loop.C

---
+++ For loop.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int i = 1; i <= 100; i += 3)   // Incrementing instead of decrementing
+    {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
