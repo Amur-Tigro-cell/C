b6675f8b8308182aea048c97f9d313e7d3e33a1c
Subro sutradhar <subroopu@gmail.com>
2025-04-20 20:48:56 +0600

    Create Variables and their decleration.C

---
+++ Variables and their decleration.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int y;
+    y = 9;
+    printf("%d\n", y);
+    y = y+500;
+    printf("%d\n", y);
+    y = y-300;
+    printf("%d", y);
+
+    return 0;
+
+}
