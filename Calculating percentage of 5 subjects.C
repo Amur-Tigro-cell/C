7949355c79f78d6aa41bf6d693c042139df7a069
Subro sutradhar <subroopu@gmail.com>
2025-04-20 23:28:37 +0600

    Create Calculating percentage of 5 subjects.C

---
+++ Calculating percentage of 5 subjects.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    float a1 = 90;
+    float a2 = 99;
+    float a3 = 88;
+    float a4 = 91;
+    float a5 = 87;
+
+    float percentage=(a1+a2+a3+a4+a5)/5;
+    printf("%f",percentage);
+
+    return 0;
+
+}
