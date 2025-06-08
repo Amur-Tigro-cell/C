8659aea4eac7d531b908a2a2273d539957e729c7
Subro sutradhar <subroopu@gmail.com>
2025-06-09 03:19:37 +0600

    Create Number pyramid must pattern .C

---
+++ Number pyramid must pattern .C
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int nst = n;
+    int nsp = 1;
+
+
+    for(int i = 1; i <= 2 * n - 1; i++) {
+        printf("%d", i);
+    }
+    printf("\n");
+
+    for(int i = 1; i <= n; i++) {
+        int a = 1;
+
+
+        for(int j = 1; j <= nst; j++) {
+            printf("%d", a++);
+        }
+
+
+        for(int k = 1; k <= nsp; k++) {
+            printf(" ");
+        }
+
+
+        for(int j = 1; j <= nst; j++) {
+            printf("%d", a++);
+        }
+
+        nst--;
+        nsp += 2;
+        printf("\n");
+    }
+
+    return 0;
+}
