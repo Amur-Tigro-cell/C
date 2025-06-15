da2aa13cddc90af26bdf4cf584e5dab24830908d
Subro sutradhar <subroopu@gmail.com>
2025-06-16 00:20:50 +0600

    Create Basic Array.C

---
+++ Basic Array.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    int arr[5]={2,4,6,8,1};
+    printf("%d",arr[2]);
+    return 0;
+}
+
+
+#include<stdio.h>
+int main(){
+    int arr[5];
+    arr[0]=1;
+    arr[1]=2;
+    arr[2]=3;
+    arr[3]=4;
+    printf("%d",arr[2]);
+    return 0;
+}
+
+
+#include<stdio.h>
+int main(){
+    int arr[5]={2,3,4,5,1};
+    for(int i = 0; i<=4; i++) printf("\n%d",arr[2]);
+
+    return 0;
+}
+
+
